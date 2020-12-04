e6923ad78ebcbd2ec4ebc08d2ad8016449b177b4
ahmedmohd957 <ahmed.mohd957@gmail.com>
2020-12-04 11:27:45 +0100

    Added Cookies Popup

--- app.js
+++ app.js
@@ -25,14 +25,41 @@ const showNav = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${i / 10}s`;
             }
         }
-
-        // navLinks.forEach((link, index) => {
-        //     if (link.style.animation) {
-        //         link.style.animation = '';
-        //     } else {
-        //         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10}s`;
-        //     }
-        // });
     });
 }
+
 showNav();
+
+// Cookies
+const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({...acc, [key.trim()]: value }), {});
+        return cookies[item];
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value};`
+    }
+}
+
+const storageType = cookieStorage;
+const consentPropertyName = 'trim_cookies_consent';
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+    const consentPopup = document.querySelector('.cookies_popup');
+    const acceptBtn = document.querySelector('.cookies_popup-btn');
+    acceptBtn.addEventListener('click', () => {
+        saveToStorage(storageType);
+        consentPopup.classList.add('hidden');
+    });
+
+    if (shouldShowPopup(storageType)) {
+        setTimeout(() => {
+            consentPopup.classList.remove('hidden');
+        }, 2000);
+    }
+};
